1ee93b442dbf5b62db590c66f7fb5c3469034cf9
Sahar Bitton <saharbi@walla.co.il>
2020-08-03 11:38:42 +0300

    Updates

---
+++ precache-manifest.fda10c8e5a7338d9abace61b21a9053b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159a7eb85e5c45d0abd330d9d8722751",
+    "url": "/smarthouse/index.html"
+  },
+  {
+    "revision": "b3e3116bff4e0f35ae3c",
+    "url": "/smarthouse/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d2158008b7ee6da02156",
+    "url": "/smarthouse/static/css/main.c0424586.chunk.css"
+  },
+  {
+    "revision": "b3e3116bff4e0f35ae3c",
+    "url": "/smarthouse/static/js/2.e2d115f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/smarthouse/static/js/2.e2d115f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2158008b7ee6da02156",
+    "url": "/smarthouse/static/js/main.c66e0fed.chunk.js"
+  },
+  {
+    "revision": "9269f0368b62973f47c9",
+    "url": "/smarthouse/static/js/runtime-main.bf80f67d.js"
+  }
+]);
